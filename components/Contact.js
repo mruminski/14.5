d501bbd3ce8a310665e107bfdc684b01fd479859
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-23 00:42:06 +0200

    Rewrite Contact to jsx

--- components/Contact.js
+++ components/Contact.js
@@ -4,37 +4,22 @@ var Contact = React.createClass({
   },
 
   render: function() {
-    return React.createElement(
-      'div',
-      { className: 'contactItem' },
-      React.createElement('img', {
-        className: 'contactImg',
-        src:
-          'https://openclipart.org/image/300px/svg_to_png/202776/pawn.png',
-        alt: 'Contact image'
-      }),
-      React.createElement(
-        'p',
-        {
-          className: 'label'
-        },
-        'First name: ' + this.props.item.firstName
-      ),
-      React.createElement(
-        'p',
-        {
-          className: 'label'
-        },
-        'Last name: ' + this.props.item.lastName
-      ),
-      React.createElement(
-        'a', 
-        {
-          className: 'emailLabel',
-          href: 'mailto:' + this.props.item.email
-        },
-        this.props.item.email
-      ),
-    );
+    return (
+      <div className={'contactItem'}>
+        <img 
+          className={'contactImg'}
+          src={'https://openclipart.org/image/300px/svg_to_png/202776/pawn.png'}
+          alt={'Contact image'}/>
+        <p className={'label'}>
+          First name: {this.props.item.firstName}
+        </p>
+        <p className={'label'}>
+          Last name: {this.props.item.lastName}
+        </p>
+        <a href={'mailto:' + this.props.item.email}>
+          {this.props.item.email}
+        </a>
+      </div>
+    )
   }
 });

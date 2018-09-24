4bd45a987e38326a7955cb1da6ca4156c945602d
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-09-24 16:20:10 +0200

    Add labels for form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,18 +6,24 @@ var ContactForm = React.createClass({
   render: function() {
     return (
       React.createElement('form', {className: 'contactForm'},
+      React.createElement('label', {htmlFor: 'fName'}, 'First name: '),
       React.createElement('input', {
         type: 'text',
+        id: 'fName',
         placeholder: 'First name',
         value: this.props.contact.firstName,
       }),
+      React.createElement('label', {htmlFor: 'lName'}, 'Last name: '),
       React.createElement('input', {
         type: 'text',
+        id: 'lName',
         placeholder: 'Last name',
         value: this.props.contact.lastName,
       }),
+      React.createElement('label', {htmlFor: 'email'}, 'Email: '),
       React.createElement('input', {
         type: 'text',
+        id: 'email',
         placeholder: 'Email',
         value: this.props.contact.email,
       }),
